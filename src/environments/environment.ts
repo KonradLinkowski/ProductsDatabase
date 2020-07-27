110419a49fbf09952368e50fb260fefda2d75888
Konrad Linkowski <linkowski.konrad97@gmail.com>
2020-07-27 19:11:11 +0200

    Add @angular/fire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,10 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { shared } from './shared';
+
 export const environment = {
+  ...shared,
   production: false
 };
 
