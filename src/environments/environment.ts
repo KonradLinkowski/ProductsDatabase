a767ba41b15120346beb970f72917ef9603df181
Konrad Linkowski <linkowski.konrad97@gmail.com>
2020-07-27 21:50:06 +0200

    Deployment fixes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,16 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-import { shared } from './shared';
-
 export const environment = {
-  ...shared,
+  firebase: {
+    apiKey: 'AIzaSyDMjQyilRZ7vPQvZemV96jk0kvGb-BQ8LU',
+    authDomain: 'productdatabase-2a003.firebaseapp.com',
+    databaseURL: 'https://productdatabase-2a003.firebaseio.com',
+    projectId: 'productdatabase-2a003',
+    storageBucket: 'productdatabase-2a003.appspot.com',
+    messagingSenderId: '1053709773453',
+    appId: '1:1053709773453:web:6302d629477024cc7ad0c4'
+  },
   production: false
 };
 
